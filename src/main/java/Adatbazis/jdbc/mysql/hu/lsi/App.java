e1841d6ae8086eb84249b8d1b30513f4c46c3b60
Endre Janák <janak.endre@gmail.com>
2016-10-08 16:09:02 +0200

    add derby memory DB and create one table

--- src/main/java/Adatbazis/jdbc/mysql/hu/lsi/App.java
+++ src/main/java/Adatbazis/jdbc/mysql/hu/lsi/App.java
@@ -2,28 +2,36 @@ package Adatbazis.jdbc.mysql.hu.lsi;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
 	
-    private static Connection connection;
-    private static Statement s;
-    private static java.sql.PreparedStatement p;
-
+	private static Connection connectionMysql;
+	private static Statement s;
+	private static PreparedStatement p;
+	
     //JDBC
 	public static void main( String[] args ) throws ClassNotFoundException, SQLException {
     	
 		try {
 			
 			//Connect to Mysql
-			connection = DriverManager.getConnection(
+			connectionMysql = DriverManager.getConnection(
 					DataBaseConf.URL+DataBaseConf.DBNAME+DataBaseConf.TZCONFIG, 
 					DataBaseConf.USERNAME,
 					DataBaseConf.PWD);
-			s = connection.createStatement();
+			Statement s = connectionMysql.createStatement();
 			
+			//Connect and create a derby memory DB
+			Connection connectionDerby = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true");
+			Statement derbystatement = connectionDerby.createStatement();
+			derbystatement.executeUpdate("CREATE TABLE MyUser( Id INTEGER, Name VARCHAR(255), "
+					+ "Password VARCHAR(255), "
+					+ "Primary key (Id))");
+					
 			//Nem ajánlott
 			String defsql = "SELECT c.cid FROM cache as c where c.cid = 'schema';";
 			ResultSet defaultresult = s.executeQuery(defsql);
@@ -33,7 +41,7 @@ public class App {
 
 			//Ajánlott
 	    	String prepsql = "SELECT c.cid FROM cache as c where c.cid = ?;";
-	    	p = connection.prepareStatement(prepsql);
+	    	p = connectionMysql.prepareStatement(prepsql);
 			p.setString(1, "schema");
 		    ResultSet prepresultset = p.executeQuery();
 	    	while (prepresultset.next()) {
@@ -44,9 +52,7 @@ public class App {
 			e.printStackTrace();
 		}
 		finally {
-			connection.close();
-			s.close();
-			p.close();			
+
 		}
     }
 }
